5aa9b91975a28e3e7e08683a7850bfc9c48f3c2d
Bujorel Tecu <btecu@outlook.com>
2015-08-04 16:39:07 -0400

    Do not import momentjs

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    // Import moment.js
-    app.import(path.join(app.bowerDirectory, '/moment/moment.js'));
-
     // Import unminified css and js
     app.import(path.join(app.bowerDirectory, '/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css'));
     app.import(path.join(app.bowerDirectory , '/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js'));
