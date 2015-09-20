71162617ccfabe98e42fc1ee764b7b39e6c02d86
Bujorel Tecu <btecu@outlook.com>
2015-09-19 23:31:36 -0400

    Update eonasdan-bootstrap-datetimepicker
    Update eonasdan-bootstrap-datetimepicker to 4.17.37

--- blueprints/ember-cli-bootstrap-datetimepicker/index.js
+++ blueprints/ember-cli-bootstrap-datetimepicker/index.js
@@ -7,7 +7,7 @@ module.exports = {
     var blueprint = this;
 
     return blueprint.addAddonToProject('ember-cli-moment-shim', '0.6.2').then(function() {
-      return blueprint.addBowerPackageToProject('eonasdan-bootstrap-datetimepicker', '4.15.35');
+      return blueprint.addBowerPackageToProject('eonasdan-bootstrap-datetimepicker', '4.17.37');
     });
   }
 };
