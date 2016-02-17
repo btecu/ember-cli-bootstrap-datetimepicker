da91a32797e24b2181873e7b925d232f6f67a12d
Bujorel Tecu <btecu@outlook.com>
2016-02-17 10:26:59 -0500

    Ember addons installed as packages do not run their own hooks (`afterInstall`)

--- blueprints/ember-cli-bootstrap-datetimepicker/index.js
+++ blueprints/ember-cli-bootstrap-datetimepicker/index.js
@@ -9,7 +9,9 @@ module.exports = {
     */
     return this.addPackagesToProject([
       { name: 'ember-cli-moment-shim', target: '1.0.0' },
-      { name: 'eonasdan-bootstrap-datetimepicker-ie ', target: '4.17.42' }
+      { name: 'eonasdan-bootstrap-datetimepicker-ie ', target: '4.17.42' },
+      { name: 'moment', target: '^2.11.2' },
+      { name: 'moment-timezone', target: '^0.5.0' }
     ]);
   }
 };
