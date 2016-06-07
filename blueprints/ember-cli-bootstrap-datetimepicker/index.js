eabd2f2e8d04e84cd6f289f80a86acdf2f8b4a7a
Bj <btecu@outlook.com>
2016-06-06 20:44:42 -0400

    Bump dependencies, fix dependency

--- blueprints/ember-cli-bootstrap-datetimepicker/index.js
+++ blueprints/ember-cli-bootstrap-datetimepicker/index.js
@@ -8,10 +8,10 @@ module.exports = {
       https://github.com/Eonasdan/bootstrap-datetimepicker/issues/1438
     */
     return this.addPackagesToProject([
-      { name: 'ember-cli-moment-shim', target: '1.0.0' },
+      { name: 'ember-cli-moment-shim', target: '1.3.0' },
       { name: 'eonasdan-bootstrap-datetimepicker-npm', target: '4.17.37' },
-      { name: 'moment', target: '^2.11.2' },
-      { name: 'moment-timezone', target: '^0.5.0' }
+      { name: 'moment', target: '^2.13.0' },
+      { name: 'moment-timezone', target: '^0.5.4' }
     ]);
   }
 };
