abbdfdff8f2f4e9c822db45a1f1a75d5b8bee968
Bujorel Tecu <btecu@outlook.com>
2016-09-12 15:30:43 -0400

    Switch back to custom version. Newest version has several major bugs.

--- blueprints/ember-cli-bootstrap-datetimepicker/index.js
+++ blueprints/ember-cli-bootstrap-datetimepicker/index.js
@@ -4,9 +4,13 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function(options) {
+    /* Use custom build until bugs are fixed
+      https://github.com/Eonasdan/bootstrap-datetimepicker/issues/1778
+      https://github.com/Eonasdan/bootstrap-datetimepicker/issues/1783
+    */
     return this.addPackagesToProject([
       { name: 'ember-cli-moment-shim', target: '2.0.0' },
-      { name: 'eonasdan-bootstrap-datetimepicker', target: '4.17.42' },
+      { name: 'eonasdan-bootstrap-datetimepicker-npm', target: '4.17.37' },
       { name: 'moment', target: '^2.15.0' },
       { name: 'moment-timezone', target: '^0.5.5' }
     ]);
