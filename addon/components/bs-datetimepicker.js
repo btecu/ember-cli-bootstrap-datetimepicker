d954a9feb2ca5be7a00462268b29259dcfaaafad
trojanware <silverspade_vaibhav@hotmail.com>
2016-11-18 12:40:03 +0530

    Fix #25 : Add observer for locale attribute to pass on changes to the plugin

--- addon/components/bs-datetimepicker.js
+++ addon/components/bs-datetimepicker.js
@@ -82,6 +82,10 @@ export default Component.extend({
     this.addObserver('minDate', function() {
       this.$().data('DateTimePicker').minDate(this.get('minDate'));
     });
+
+    this.addObserver('locale', function() {
+      this.$().data('DateTimePicker').minDate(this.get('locale'));
+    });
   },
 
   willDestroyElement() {
@@ -89,6 +93,7 @@ export default Component.extend({
     this.removeObserver('date');
     this.removeObserver('maxDate');
     this.removeObserver('minDate');
+    this.removeObserver('locale');
 
     // Running the `ember` application embedded might cause the DOM to be cleaned before
     let dateTimePicker = this.$().data('DateTimePicker');
