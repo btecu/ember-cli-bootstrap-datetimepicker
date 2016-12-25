301c95aa4dff24f6ffbec0869447021e5cef4a73
Bj <btecu@outlook.com>
2016-12-25 12:04:23 -0500

    Make compatible with jQuery 3. Fixes #21. Fixes #28.
    Use `eonasdan-bootstrap-datetimepicker-npm` (custom build) that is compatible with jQuery 3.

--- blueprints/ember-cli-bootstrap-datetimepicker/index.js
+++ blueprints/ember-cli-bootstrap-datetimepicker/index.js
@@ -9,10 +9,10 @@ module.exports = {
       https://github.com/Eonasdan/bootstrap-datetimepicker/issues/1783
     */
     return this.addPackagesToProject([
-      { name: 'ember-cli-moment-shim', target: '2.2.1' },
-      { name: 'eonasdan-bootstrap-datetimepicker-npm', target: '4.17.37' },
+      { name: 'ember-cli-moment-shim', target: '3.0.1' },
+      { name: 'eonasdan-bootstrap-datetimepicker-npm', target: '4.17.42' },
       { name: 'moment', target: '^2.17.1' },
-      { name: 'moment-timezone', target: '^0.5.10' }
+      { name: 'moment-timezone', target: '^0.5.11' }
     ]);
   }
 };
