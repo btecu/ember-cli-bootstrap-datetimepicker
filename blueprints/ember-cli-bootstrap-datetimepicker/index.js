907f09745a206b4d17c54953d21c33ab096fa479
Bujorel Tecu <btecu@outlook.com>
2017-05-29 13:44:44 -0400

    Update ember-cli and dependencies. Kill bower. Add `yarn` lock.

--- blueprints/ember-cli-bootstrap-datetimepicker/index.js
+++ blueprints/ember-cli-bootstrap-datetimepicker/index.js
@@ -3,16 +3,16 @@ module.exports = {
 
   normalizeEntityName: function() {},
 
-  afterInstall: function(options) {
+  afterInstall: function() {
     /* Use custom build until bugs are fixed
       https://github.com/Eonasdan/bootstrap-datetimepicker/issues/1778
       https://github.com/Eonasdan/bootstrap-datetimepicker/issues/1783
     */
     return this.addPackagesToProject([
-      { name: 'ember-cli-moment-shim', target: '3.0.1' },
+      { name: 'ember-cli-moment-shim', target: '3.3.0' },
       { name: 'eonasdan-bootstrap-datetimepicker-npm', target: '4.17.42' },
-      { name: 'moment', target: '^2.17.1' },
-      { name: 'moment-timezone', target: '^0.5.11' }
+      { name: 'moment', target: '^2.18.1' },
+      { name: 'moment-timezone', target: '^0.5.13' }
     ]);
   }
 };
