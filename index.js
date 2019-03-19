339e75c16fe520c18e12c73ca12f0dc4c17513ff
Bujorel Tecu <btecu@outlook.com>
2019-03-19 16:12:33 -0700

    Use `npm` package instead of the repository directly. Closes #65.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
     this._super.included(app);
 
     // Import unminified css and js
-    let basePath = 'node_modules/eonasdan-bootstrap-datetimepicker';
+    let basePath = 'node_modules/btecu-eonasdan-bootstrap-datetimepicker';
     app.import(`${basePath}/build/css/bootstrap-datetimepicker.css`);
     app.import(`${basePath}/src/js/bootstrap-datetimepicker.js`);
   }
